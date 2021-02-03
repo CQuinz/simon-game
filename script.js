23f449c697ce3d87bd7b4a32c12800aea15bd4c6
Christian <christian.quinlan36@gmail.com>
2021-02-03 20:50:33 +0000

    added further constants for html elements

---
+++ script.js
@@ -0,0 +1,16 @@
+//******* VARIABLES***********
+let newGame = false;
+let sequenceNum;
+let roundNum = 0;
+let colorArray =[];
+
+// *******CONSTANTS***********
+// Colorboxes
+const red = document.getElementById('red');
+const yellow = document.getElementById('yellow');
+const blue = document.getElementById('blue');
+const green = document.getElementById('green');
+
+// Button and text elements
+const startBtn = document.getElementById('start');
+const roundNumText = document.getElementById('roundNumText');
