46d46a7def8aadba8c60e2cb5c71f5f57f85b1ee
Christian <christian.quinlan36@gmail.com>
2021-02-04 19:52:17 +0000

    adding colorBoxes array, adding eventListeners for playAnimation function

--- script.js
+++ script.js
@@ -6,11 +6,21 @@ let colorArray =[];
 
 // *******CONSTANTS***********
 // Colorboxes
-const red = document.getElementById('red');
-const yellow = document.getElementById('yellow');
-const blue = document.getElementById('blue');
-const green = document.getElementById('green');
+// const red = document.getElementById('red');
+// const yellow = document.getElementById('yellow');
+// const blue = document.getElementById('blue');
+// const green = document.getElementById('green');
+const colorShapes
 
 // Button and text elements
 const startBtn = document.getElementById('start');
 const roundNumText = document.getElementById('roundNumText');
+
+// EventListeners for color boxes
+
+
+// Play Function
+
+let playAnimation = (color)=>{
+
+}
