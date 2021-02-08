cf82ac69a4ab13c2f395c7e75b09d925739a302b
Christian <christian.quinlan36@gmail.com>
2021-02-08 20:45:36 +0000

    Created a new callback function animatePlayColor to take a color parameter and play the coresponding color and sound. also renamed play function to getPlayerSelectedColor to be able to pass players color selection into the animatePlayColor function

--- script.js
+++ script.js
@@ -19,34 +19,34 @@ const roundNumText = document.getElementById('roundNumText');
 // Generate random color
 const randomColor = ()=>{
   let color = Math.floor(Math.random()*4);
-  console.log(colorArray[color]);
+  return(colorArray[color]);
 }
 
-
-// Play Function
-
-let play = (e)=>{
-  let color =e.srcElement;
+const animatePlayColor = (color)=>{
   // Play animation
   color.classList.add('full-color');
   // Play sound
-  // let colorID = "'" + color.id + "'";
-  let colorID =  color.id;
   let sound = new Audio(`${colorID}.mp3`);
-  // let sound = new Audio("blue.mp3");
   sound.play();
-  console.log(colorID);
+  // Remove 'full-color' class from div after 0.3 seconds
   setInterval(()=>{
     color.classList.remove('full-color');
   }, 300);
-  
+}
 
+// Get player selected color function
+let getPlayerSelectedColor = (e)=>{
+  let color =e.srcElement;
+ 
+  let colorID =  color.id;
+  console.log(colorID);
+  
 }
 
 
 // EventListeners for color boxes
 for(i=0;i<colorBoxes.length;i++){
   
-  colorBoxes[i].addEventListener('click',play);
+  colorBoxes[i].addEventListener('click',getPlayerSelectedColor);
  
 }
