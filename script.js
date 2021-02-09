ceb06c1c0f5077c097378be2dcca7acad43d60ae
Christian <christian.quinlan36@gmail.com>
2021-02-09 20:13:51 +0000

    changed color variable in animatePlayColor function to color.id to select the the color divs id

--- script.js
+++ script.js
@@ -26,8 +26,9 @@ const animatePlayColor = (color)=>{
   // Play animation
   color.classList.add('full-color');
   // Play sound
-  let sound = new Audio(`${colorID}.mp3`);
+  let sound = new Audio(`${color.id}.mp3`);
   sound.play();
+
   // Remove 'full-color' class from div after 0.3 seconds
   setInterval(()=>{
     color.classList.remove('full-color');
@@ -38,11 +39,12 @@ const animatePlayColor = (color)=>{
 let getPlayerSelectedColor = (e)=>{
   let color =e.srcElement;
  
-  let colorID =  color.id;
-  console.log(colorID);
+  // let colorID =  color.id;
+  // console.log(colorID);
+
+  animatePlayColor(color);
   
 }
-
 
 // EventListeners for color boxes
 for(i=0;i<colorBoxes.length;i++){
